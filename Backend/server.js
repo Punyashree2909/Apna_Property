e44e672cf0ec1f74b21f00b4d571bb2303e3c4e6
Punyashree P wanare <217946513+Punyashree2909@users.noreply.github.com>
2025-11-03 19:19:29 +0530

    backend setup

---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Base test route
+app.get("/", (req, res) => {
+  res.send("Backend setup running successfully 🚀");
+});
+
+// (Future routes)
+// import propertyRoutes from "./routes/propertyRoutes.js";
+// app.use("/api/properties", propertyRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
